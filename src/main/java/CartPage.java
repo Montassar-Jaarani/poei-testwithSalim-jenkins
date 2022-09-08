eabaa82a193407f3c7538a3ca593417cf86b0d03
Montassar Jaarani <montassar.jaarani@esprit.tn>
2022-09-08 16:43:44 +0200

    Cart page avec les assertions capacite couleur config sous total

--- src/main/java/CartPage.java
+++ src/main/java/CartPage.java
@@ -1,6 +1,59 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class CartPage {
 
     private WebDriver driver;
+    private By dropDownSelector = By.cssSelector("#quantity");
+    private By firstProdColorSelector = By.cssSelector(".a-text-bold + span");
+    private By firstProductCapSelector = By.cssSelector(".a-text-bold + span");
+    private By firstProdConfigSelector = By.cssSelector(".a-text-bold + span");
+    private By subTotalCartSelector = By.cssSelector("#sc-subtotal-label-activecart");
+
+
+    private final int TIMEOUT_COOKIE = 6;
+
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void selectQuantity(int quantity) {
+        WebElement dropdown = driver.findElement(dropDownSelector);
+        Select quantiteDropdown = new Select(dropdown);
+        quantiteDropdown.selectByIndex(2);
+    }
+
+    public String getFirstProductCapacity(){
+        List<WebElement> informations = driver.findElements(firstProductCapSelector);
+        WebElement taille = informations.get(0);
+        return taille.getText();
+    }
+
+    public String  getFirstProductColor() {
+        List<WebElement> informations = driver.findElements(firstProdColorSelector);
+
+        WebElement couleur = informations.get(1);
+        return couleur.getText();
+    }
+
+    public String  getFirstProductConfiguration() {
+        List<WebElement> informations = driver.findElements(firstProdConfigSelector);
+        WebElement configuration = informations.get(2);
+        return configuration.getText();
+    }
+
+    public String  getSubtotalCart() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        WebElement soustotale = wait.until(ExpectedConditions.visibilityOfElementLocated(subTotalCartSelector));
+        return soustotale.getText();
+    }
+
 }
--- src/test/java/TpPageObject.java
+++ src/test/java/TpPageObject.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,19 +24,48 @@ public class TpPageObject {
 
     @Test
     public void testPO() {
+        // Arrange/////////////////////
+        String keywordHomePgae="Apple iPhone 13 Pro Max (256 Go) - Vert Alpin";
+        int indexSearchResult = 0;
+        int indexSelectQuantity = 2;
+
+        final String expectedTaille  = "256Go";
+        final String expectedCouleur  = "Vert alpin";
+        final String expectedConfiguration  = "Sans AppleCare+";
+        final String expectedTotale  = "Sous-total (2 articles):";
+        final String searchKeyword = "Apple iPhone 13 Pro Max (256 Go) - Vert Alpin";
+
+        ///Act////////////////////////////
         HomePage homePage = new HomePage(driver);
         homePage.acceptCookie();
-        homePage.searchWithButton( "Apple iPhone 13 Pro Max (256 Go) - Vert Alpin");
+        homePage.searchWithButton( keywordHomePgae);
 
 
         SearchResultPage searchResultPage = new SearchResultPage(driver);
-        searchResultPage.openSearchResult(0);
+        searchResultPage.openSearchResult(indexSearchResult);
 
 
         ProductPage productPage = new ProductPage(driver);
         productPage.addToCart();
         productPage.refuseAppleCare();
         productPage.openCart();
+
+        CartPage cartPage =new CartPage(driver);
+        cartPage.selectQuantity(indexSelectQuantity);
+
+
+        cartPage.getFirstProductCapacity();
+        cartPage.getFirstProductColor();
+        cartPage.getFirstProductConfiguration();
+        cartPage.getSubtotalCart();
+
+
+        ///////////////////////////////ASSERT///////////////////////////
+
+        Assert.assertEquals(cartPage.getFirstProductCapacity(),expectedTaille,"Taille pas bon");
+        Assert.assertEquals(cartPage.getFirstProductColor(),expectedCouleur ,"couleur pas bon");
+        //Assert.assertEquals(cartPage.getFirstProductConfiguration(),"config pas bon");
+        //Assert.assertEquals(cartPage.getSubtotalCart(),expectedTotale,"sous total pa bon");
 
     }
 }
