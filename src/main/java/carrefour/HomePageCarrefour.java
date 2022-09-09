a097b3ca1514f0dcbcd9b7488f36c98a6b434d7b
Montassar Jaarani <montassar.jaarani@esprit.tn>
2022-09-09 14:23:00 +0200

    added folder amazon et carrefour, created TpCarrefour et HomePageCarrefour

---
+++ src/main/java/carrefour/HomePageCarrefour.java
@@ -0,0 +1,14 @@
+package carrefour;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePageCarrefour {
+
+    private WebDriver driver;
+
+    public HomePageCarrefour(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+}
